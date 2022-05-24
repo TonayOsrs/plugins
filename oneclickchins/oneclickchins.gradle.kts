4c6b0770b19b25d4f27b812c2ab3ddab0d2d35b7
TonayOsrs <106201163+TonayOsrs@users.noreply.github.com>
2022-05-24 19:48:59 -0400

    Update oneclickchins.gradle.kts

--- oneclickchins/oneclickchins.gradle.kts
+++ oneclickchins/oneclickchins.gradle.kts
@@ -49,7 +49,6 @@ tasks {
             attributes(mapOf(
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
-                    "Plugin-Provider" to project.extra["PluginProvider"],
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
             ))
